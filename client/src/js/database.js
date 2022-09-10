1d490f6ee25130ce06ed51a0c83568f9e58934df
Elena Carey <theelenacarey@gmail.com>
2022-09-09 21:42:29 -0500

    wtf

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,52 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('wyrd_db', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('wyrd')) {
+        console.log('wyrd database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('wyrd', { keyPath: 'id', autoIncrement: true });
+      console.log('wyrd database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  
+  const wyrdDb = await openDB('wyrd_db', 1);
+
+  const tx = wyrdDb.transaction('wyrd', 'readwrite');
+
+  const store = tx.objectStore('wyrd');
+
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("GET from the database");
+
+	//connection to IndexedDB database and version.
+	const wyrdDb = await openDB("wyrd_db", 1);
+
+	//create new transaction and which store/data privilages
+	const tx = wyrdDb.transaction("wyrd", "readonly");
+
+	//Open up desired object store.
+	const store = tx.objectStore("wyrd");
+
+	//Use the .getAll() method to get all data in database
+	const request = store.getAll();
+
+	//GET confirmation of the request
+	const result = await request;
+	console.log("result.value", result);
+	return result;
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,13 +15,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "WyrdWright",
+      }),
+      new InjectManifest({
+        swSrc: "./src/src-sw.js",
+        swDest: "./src/src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        name: "WyrdWright Application",
+        short_name: "WyrdWright",
+        description: "Text editor online!",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { targets: "defaults" }]],
+            },
+          },
+        },
       ],
     },
-  };
-};
+  }
+}
